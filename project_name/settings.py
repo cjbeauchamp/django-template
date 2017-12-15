0e0e24b65f709942dcde4c62e6076b8741dcc9ad
Chris Beauchamp <cjbeauchamp@gmail.com>
2017-12-15 13:20:58 -0500

    log request id

--- project_name/settings.py
+++ project_name/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'log_request_id.middleware.RequestIDMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
