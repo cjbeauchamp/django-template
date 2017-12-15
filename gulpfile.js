86ae54514408e0ebb4621164ad076ba170433709
Chris Beauchamp <cjbeauchamp@gmail.com>
2017-12-15 13:09:31 -0500

    web structure

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var root_dir = "./{{ project_name }}/static"
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+gulp.task('sass', function() {
+  return gulp.src(root_dir + '/sass/*.scss')
+    .pipe(concat('main.css'))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(root_dir));
+});
+
+gulp.task('sass:watch', function() {
+  gulp.watch([root_dir + '/sass/**/*.scss'], ['sass']);
+});
+
+gulp.task('js', function() {
+  return gulp.src(root_dir + '/js/*.js')
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest(root_dir));
+});
+
+gulp.task('js:watch', function() {
+  gulp.watch([root_dir + '/js/*.js'], ['js']);
+});
--- starter.sh
+++ starter.sh
@@ -1,6 +1,11 @@
 read -p "Enter the app name => du-" APP_NAME_SUFFIX
 
-django-admin.py startproject --template=https://github.com/cjbeauchamp/django-template/archive/master.zip --name=Procfile ${APP_NAME_SUFFIX}
+django-admin.py startproject \
+	--template=https://github.com/cjbeauchamp/django-template/archive/master.zip \
+	--name=Procfile \
+	--name=gulpfile.js \
+	${APP_NAME_SUFFIX}
+
 cd ${APP_NAME_SUFFIX}
 virtualenv venv
 source venv/bin/activate
