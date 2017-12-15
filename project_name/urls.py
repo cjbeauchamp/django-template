c8c17e249b77246cec24d73a0d7a5b5f0a9fecc4
Chris Beauchamp <cjbeauchamp@gmail.com>
2017-12-15 13:26:00 -0500

    python cleanup

--- project_name/urls.py
+++ project_name/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 import api.urls
+import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
